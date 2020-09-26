de2072883890cbc0a64fd73c6b0a6a2541498058
wuerping1 <wuerping1@yy.com>
2020-09-26 11:54:59 +0800

    support ioutil.ReadDir

---
+++ pkger_ioutil.go
@@ -0,0 +1,24 @@
+package pkger
+
+import (
+	"os"
+	"sort"
+
+	"github.com/markbates/pkger"
+)
+
+// ReadDir reads the directory named by dirname and returns
+// a list of directory entries sorted by filename.
+func ReadDir(dirname string) ([]os.FileInfo, error) {
+	f, err := pkger.Open(dirname)
+	if err != nil {
+		return nil, err
+	}
+	list, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	sort.Slice(list, func(i, j int) bool { return list[i].Name() < list[j].Name() })
+	return list, nil
+}
